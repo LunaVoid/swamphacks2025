edf343c47f1cdf76e280c68bcc20d1b6ec3679fc
Luna <26190850+LunaVoid@users.noreply.github.com>
2025-01-26 09:59:13 -0500

    added logo

--- frontend/FirePower/src/components/Navbar.jsx
+++ frontend/FirePower/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 
 import React from 'react'
-
+import firelogo from "../assets/firelogo.jpg"
 function Navbar() {
 return (
         <nav className='flex bg-gray-800 text-white flex-col h-1/3'>
             <ul className='flex flex-row space-x-8'>
+                <li><img className = "logo"src = {firelogo}></img></li>
                 <li><a href = "/dash">Dashboard</a></li>
                 <li><a href = "/camera">Camera</a></li>
             </ul>
